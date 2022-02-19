50a453f81fa87a9715c7bdd1c103205532ad236a
itemsis <emerson@itemsistemas.com.br>
2022-02-18 22:04:22 -0300

    First commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Documentação ERP - Item Sistemas',
+  description: 'Documentação ERP - Item Sistemas',
+  lang: 'pt-BR',
+  themeConfig: {
+      nav: [
+          { text: 'Home', link: '/'}
+      ],
+      sidebar: [{text: 'Home', link: '/'}]
+  }
+}
